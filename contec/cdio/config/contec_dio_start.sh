733b37e4b211fd1113abf04f9e18c0820d38a8ec
Yasuyuki Nakamura <ynakamura@forevision.co.jp>
2022-02-18 09:17:39 +0900

    new version of contec driver

--- contec/cdio/config/contec_dio_start.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-modprobe cdiousb
-cp -f 180-cdiousb.rules /etc/udev/rules.d
-cp -f contec_dio.conf /usr/local/etc/contec_dio.conf
-#end
--- contec/cdio/config/contec_dio_stop.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-rmmod cdiousb
-rm -f /etc/udev/rules.d/180-cdiousb.rules
-rm -f /usr/local/etc/contec_dio.conf
-#end
